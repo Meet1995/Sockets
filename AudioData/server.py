d26e507d7811011d666a222f544ec868e7d5ee73
Meet <meetgandhi14@gmail.com>
2020-04-28 12:32:09 +0530

    Create server.py

---
+++ AudioData/server.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  5 16:17:33 2020
+
+@author: mg21929
+"""
+
+import socket
+import time
+import os
+import pyaudio
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+from scipy.io import wavfile
+
+IP = '192.168.43.115'
+PORT = 1234
+HEADERSIZE = 15
+BUFFERSIZE = 2000
+
+
+def speak(CHUNK=4000,SAMPLERATE=12000,seconds_to_wait=2,formatt='int16',
+          write_to_wav=True, get_devices=False):
+    p = pyaudio.PyAudio()
+    if get_devices:
+        for x in range(p.get_device_count()):
+            info = p.get_device_info_by_index(x)
+            print(info['name'])
+
+    CHANNELS=1
+    unpack_format= formatt #'int16'
+    if formatt == 'int16':
+        FORMAT = pyaudio.paInt16
+        THRESHHOLD = 3000
+    elif formatt == 'int32':
+        FORMAT = pyaudio.paInt32
+        THRESHHOLD = 9*10**7
+    elif formatt == 'float32':
+        FORMAT = pyaudio.paFloat32
+        THRESHHOLD = 0.09
+    else:
+        print('Data format not available, taking float32')
+        FORMAT = pyaudio.paFloat32
+
+    stream = p.open(format=FORMAT,channels=CHANNELS,rate=SAMPLERATE,
+                    input=True,output=True,frames_per_buffer=CHUNK)
+
+    chunks_per_sec = int(SAMPLERATE/CHUNK)
+
+    k=1
+    fig, ax = plt.subplots()
+    x = np.arange(CHUNK)
+    line, = ax.plot(x,np.random.randn(CHUNK))
+    if formatt == 'int16':
+        ax.set_ylim([-2**15,(2**15)-1])
+    elif formatt == 'int32':
+        ax.set_ylim([-2**31,(2**31)-1])
+    elif formatt == 'float32':
+        ax.set_ylim([-1,1])
+
+    keep_recording = True
+    start_recording = False
+    
+    while keep_recording:
+        tmp = np.frombuffer(stream.read(CHUNK), unpack_format)
+        if tmp.max() > THRESHHOLD:
+            start_recording = True
+            all_data = tmp
+            print('Recording Starts')
+            while start_recording:
+                data = stream.read(CHUNK)
+                if k == 1:
+                    raw_data = data
+                else:
+                    raw_data += data
+
+                data_unpacked = np.frombuffer(data, unpack_format)
+                line.set_ydata(data_unpacked)
+                fig.canvas.draw()
+                fig.canvas.flush_events()
+
+                all_data = np.concatenate((all_data,data_unpacked),axis=0)
+
+                k +=1
+                if k>seconds_to_wait*chunks_per_sec:
+                    idx = seconds_to_wait*chunks_per_sec*CHUNK
+                    max_value = all_data[-idx:].max()
+                    if max_value<THRESHHOLD:
+                        print('Recording Stops')
+                        plt.close()
+                        start_recording = False
+                        keep_recording = False   
+        else:
+            line.set_ydata(tmp)
+            fig.canvas.draw()
+            fig.canvas.flush_events()            
+            
+    if write_to_wav:
+        duration = int(len(all_data)/SAMPLERATE)
+        name = f'audio_{duration}_{formatt}.wav'
+        wavfile.write(name,SAMPLERATE, all_data)
+        print(f'file {name} saved to working directory')
+        return all_data, SAMPLERATE, name, raw_data
+    else:
+        return all_data, SAMPLERATE, raw_data
+
+arr,sr,nm,raw_data = speak()
+dictt = {'arr':arr,'sr':sr, 'name':nm}
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# AF_INET--> IPv4
+# SOCK_STREAM --> streaming socket
+s.bind((IP,PORT))
+s.listen()
+clientsocket, address = s.accept()
+print("Connection from:",address)
+
+def send_data(websocket, dictt, header_len):
+    msg = pickle.dumps(dictt)
+    msg = bytes(f'{len(msg):<{header_len}}', 'utf-8') + msg
+    s_t = time.time()
+    bytes_sent = websocket.send(msg)
+    websocket.recv(1)
+    time_taken = time.time() - s_t
+    return bytes_sent, time_taken
+
+def receive_data(websocket, buffer_size, header_len):
+    full_msg = b''
+    msg_not_recieved = True
+    s_t = time.time()
+    while msg_not_recieved:
+        msg = websocket.recv(buffer_size) # 8 bytes is the buffer size, keep it atleast equal to headersize
+        if full_msg == b'':
+            msglen = int(msg[:header_len].strip())
+            print(msglen)
+
+        full_msg += msg
+
+        msglen_recvd = len(full_msg)-header_len
+
+        if msglen_recvd == msglen:
+            websocket.send(bytes('1','utf-8'))
+            time_taken = time.time()-s_t
+            print('-------------------Full msg recvd---------------------')
+            dictt = pickle.loads(full_msg[header_len:])
+            print('Received array shape:',dictt['arr'].shape)
+            print('---------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxx------------------------')
+            msg_not_recieved = False
+    return msglen+buffer_size, time_taken, dictt
+
+d_s,t_s = send_data(clientsocket, dictt, HEADERSIZE)
